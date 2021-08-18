cbdbe807f3c550979c698dd372d27a11bdce676e
xinghua <854592400@qq.com>
2021-08-18 17:48:19 +0800

    配置数据源，连接mysql数据库

---
+++ src/main/java/com/xing/studyboot/database/datasource/DataSourceShow.java
@@ -0,0 +1,5 @@
+package com.xing.studyboot.database.datasource;
+
+public class DataSourceShow {
+
+}
--- src/main/java/com/xing/studyboot/rest/controller/ShowValueController.java
+++ src/main/java/com/xing/studyboot/rest/controller/ShowValueController.java
@@ -1,5 +1,10 @@
 package com.xing.studyboot.rest.controller;
 
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class ShowValueController {
+	
+	@Autowired
+	private DataSource dataSource;
 	
 //	@Value(value="${local.server.port}")
 //	private String localServerPort;
@@ -47,6 +55,15 @@ public class ShowValueController {
 		str.append("my.uuid->"+myUUid+"<br/>");
 		str.append("my.number.less.than.ten->"+myNumberLessThanTen+"<br/>");
 		str.append("my.my.number.in.range->"+myNumberInRange+"<br/>");
+		return str.toString();
+		
+	}
+	
+	@RequestMapping(value="/showDataSource")
+	public String showDataSource() throws SQLException {
+		StringBuilder str = new StringBuilder();
+		str.append("数据源信息    ->"+dataSource+"<br/>");
+		str.append("数据连接信息->"+dataSource.getConnection()+"<br/>");
 		return str.toString();
 	}
 }
