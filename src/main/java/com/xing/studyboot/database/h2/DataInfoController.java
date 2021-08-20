15ba96f78715c70e0f2dbec1b07b3d3fac9c4091
xinghua <854592400@qq.com>
2021-08-20 16:11:10 +0800

    配置数据源，连接mysql数据库 优化2

--- src/main/java/com/xing/studyboot/database/h2/DataInfoController.java
+++ src/main/java/com/xing/studyboot/database/h2/DataInfoController.java
@@ -25,8 +25,8 @@ public class DataInfoController {
 	@Qualifier(value="cjkJdbcTemplate")
     private JdbcTemplate cjkJdbcTemplate;
 
-	@Autowired
-    private DataInfoRepository dataInfoRepository;
+//	@Autowired
+//    private DataInfoRepository dataInfoRepository;
 	
 	/**
 	 * 保存数据
@@ -39,27 +39,27 @@ public class DataInfoController {
 		return "ok";
 	}
 	
-	/**
-	 * 保存数据
-	 * @return
-	 */
-    @RequestMapping("/save")
-    public String save(){
-    	dataInfoRepository.save(new DataInfo("xing", "18"));
-    	dataInfoRepository.save(new DataInfo("hua", "18"));
-    	dataInfoRepository.save(new DataInfo("留言", "18"));
-        dataInfoRepository.save(new DataInfo("苍劲空", "18"));
-        dataInfoRepository.save(new DataInfo("小伊", "18"));
-        return"save ok";
-    }
-
-    /**
-     * 获取所有数据
-     * @return
-     */
-    @RequestMapping("/findAll")
-    public Iterable<DataInfo> findAll(){
-        // 内存数据库操作 
-       return dataInfoRepository.findAll();
-    }
+//	/**
+//	 * 保存数据
+//	 * @return
+//	 */
+//    @RequestMapping("/save")
+//    public String save(){
+//    	dataInfoRepository.save(new DataInfo("xing", "18"));
+//    	dataInfoRepository.save(new DataInfo("hua", "18"));
+//    	dataInfoRepository.save(new DataInfo("留言", "18"));
+//        dataInfoRepository.save(new DataInfo("苍劲空", "18"));
+//        dataInfoRepository.save(new DataInfo("小伊", "18"));
+//        return"save ok";
+//    }
+//
+//    /**
+//     * 获取所有数据
+//     * @return
+//     */
+//    @RequestMapping("/findAll")
+//    public Iterable<DataInfo> findAll(){
+//        // 内存数据库操作 
+//       return dataInfoRepository.findAll();
+//    }
 }
