13745d732b9b76d9a2069a2b484f5353c3a0faf4
xinghua <854592400@qq.com>
2023-01-03 16:40:51 +0800

    ### 集成log4j2日志 100%

--- src/main/java/com/xing/studyboot/config/webService/WebServiceConfig.java
+++ src/main/java/com/xing/studyboot/config/webService/WebServiceConfig.java
@@ -1,24 +1,24 @@
-package com.xing.studyboot.config.webService;
-
-import javax.xml.ws.Endpoint;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import com.sun.xml.internal.ws.transport.http.server.EndpointImpl;
-import com.xing.studyboot.webService.MyService;
-import com.xing.studyboot.webService.impl.MyServiceImpl;
-
-@Configuration
-public class WebServiceConfig {
-	/**
-	 * 创建webSocket的 Endpoint
-	 * @return
-	 */
-//	@Bean
-//	public Endpoint getEndpoint() {				
-//		MyService serviceImpl=new MyServiceImpl();
-//		Endpoint publish = EndpointImpl.publish("http://localhost:8099/hello",serviceImpl);
-//		System.out.println("发布服务成功...");
-//		return publish;
-//	}
-}
-
+//package com.xing.studyboot.config.webService;
+//
+//import javax.xml.ws.Endpoint;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import com.sun.xml.internal.ws.transport.http.server.EndpointImpl;
+//import com.xing.studyboot.webService.MyService;
+//import com.xing.studyboot.webService.impl.MyServiceImpl;
+//
+//@Configuration
+//public class WebServiceConfig {
+//	/**
+//	 * 创建webSocket的 Endpoint
+//	 * @return
+//	 */
+////	@Bean
+////	public Endpoint getEndpoint() {
+////		MyService serviceImpl=new MyServiceImpl();
+////		Endpoint publish = EndpointImpl.publish("http://localhost:8099/hello",serviceImpl);
+////		System.out.println("发布服务成功...");
+////		return publish;
+////	}
+//}
+//
